69a5ca2215065be5304bacbb7df56db97f87def8
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-18 23:51:22 -0600

    Selection sort

--- algoritmos_y_estructuras/insertion_sort.py
+++ algoritmos_y_estructuras/insertion_sort.py
@@ -1,3 +1,15 @@
-lista = [10, 9, 5, 8]
+# -*- coding: utf-8 -*-
 
+lista = list(range(1, 1000000))
+ordenada = []
 
+while lista:
+    menor = lista[0]
+    for elem in lista[1:]:
+        if elem < menor:
+            menor = elem
+    ordenada.append(menor)
+    lista.remove(menor)
+    print(lista[0])
+
+print(ordenada)
