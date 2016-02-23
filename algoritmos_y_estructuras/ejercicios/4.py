382611f35086c07bc169f1a90f0448a4cf834dba
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-22 20:32:54 -0600

    vowels and consonants

---
+++ algoritmos_y_estructuras/ejercicios/4.py
@@ -0,0 +1,22 @@
+'''
+Un programa que reciba un número indeterminado de
+letras (mayúsculas o minúsculas) e indique cuántas
+vocales y cuántas consonantes entraron.
+'''
+
+
+def vowels_and_consonants(letters):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowels_count = 0
+    consonant_count = 0
+    for l in letters:
+        if l in vowels:
+            vowels_count += 1
+        else:
+            consonant_count += 1
+
+    print("Número de vocales: " + str(vowels_count) +
+          "\nNúmero de consonantes:" + str(consonant_count))
+
+
+vowels_and_consonants("aeiouaeioulll")
