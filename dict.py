965be30e8953a951a1812cc1082251cbb7a5ace1
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-22 23:36:18 -0600

    Dict example and main

---
+++ dict.py
@@ -0,0 +1,36 @@
+emilio = {
+ 'bebida': 'whiskey',
+ 'carrera': 'ciencia politica',
+ 'hermano': 'julio',
+ 'materias': [
+    'historia',
+    'matematicas',
+    'economia',
+    'fisica'
+    ],
+ 'correo': 'emilioramirezz@hotmail.com',
+ 'color': 'azul',
+ 'telefono': '5565591168',
+ 'edad': 26,
+ 'domicilio': {
+    'calle': 'calle',
+    'numero': '90',
+    'cp': 9820,
+
+ },
+ 'hobbies': [
+    'crossfit',
+    'programar',
+    'cine']
+}
+
+
+ emilio["direccion"]["cp"]
+
+ emilio["hobbies"][1]["hola"][3]
+
+
+
+
+
+
---
+++ function_main.py
@@ -0,0 +1,14 @@
+# factorial
+# n! 
+# n! = n * (n-1)!
+# 0! = 1
+# n! = 1..n
+
+# 5 = 5 * 4 * 3 * 2 * 1
+
+def funcion():
+    pass
+def main():
+    funcion()
+main()
+
