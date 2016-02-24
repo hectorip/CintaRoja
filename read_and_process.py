b0ab9cc0fd5999794282186e42a8ece82a17a27d
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-02-23 23:54:33 -0600

    Example on how to process a file

---
+++ read_and_process.py
@@ -0,0 +1,10 @@
+import sys
+
+file = open(sys.argv[1], 'r')
+for t in file:
+    # 2, 5
+    if t.strip() != '':
+        vals = t.split(",")  # ["2", " 5"]
+        number_list = [int(x.strip()) for x in vals if (x != '' or x != ' ')]
+
+    print(number_list)
